228ec01922e7009b83dd118d8d2313667f8f4164
Patrick Franco <patrickxfranco@gmail.com>
2025-06-15 16:16:02 -0300

    Add a theme provider
    Adds ThemeProvider for theme management and updates rendering in the main component.

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { createRoot } from 'react-dom/client';
 
+import { ThemeProvider } from './components/ThemeProvider';
+
 const RootElement: HTMLElement = document.getElementById('root')!;
 
-createRoot(RootElement).render(<>Olá Mundo</>);
+createRoot(RootElement).render(<ThemeProvider>Hello World</ThemeProvider>);
