9736e0b3caf5b11e2813802dc33449fae46e53df
Patrick Franco <patrickxfranco@gmail.com>
2025-06-19 00:10:44 -0300

    Corrige a chave do elemento <li> no componente Footer e melhora a estrutura do layout, adicionando um contêiner para os links externos e ajustando a formatação do texto.

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -8,8 +8,8 @@ export function Footer(): ReactElement {
   function renderExternalLinks(links: ExternalLinkProps[]): ReactElement[] {
     return links.map((link: ExternalLinkProps, index: number) => {
       return (
-        <li>
-          <Link to={link.url} title={link.title} key={index} tabIndex={index} target="_blank" className="*:size-4">
+        <li key={index}>
+          <Link to={link.url} title={link.title} tabIndex={index} target="_blank" className="*:size-4">
             {link.icon}
             <span className="sr-only">{link.title}</span>
           </Link>
@@ -19,13 +19,49 @@ export function Footer(): ReactElement {
   }
 
   return (
-    <footer className="bg-white/90 dark:bg-black/90 backdrop-blur-md mt-8 p-4">
-      <span className="text-muted-foreground text-sm">
-        Copyright © {new Date().getFullYear()} Nutshell, todos os direitos reservados.
-      </span>
-      <ul className="flex flex-row gap-4 mt-4 max-w-min *:hover:text-foreground *:text-muted *:transition-all">
-        {renderExternalLinks(links)}
-      </ul>
+    <footer className="bg-white/90 dark:bg-black/90 backdrop-blur-md mt-8 p-4 py-6">
+      <span className="text-muted text-xs">Copyright © {new Date().getFullYear()} Nutshell, todos os direitos reservados.</span>
+      <div className="flex flex-col gap-4 w-full">
+        <ul className="flex flex-row gap-4 mt-4 max-w-min *:hover:text-foreground *:text-muted *:transition-all">
+          {renderExternalLinks(links)}
+        </ul>
+        <p className="text-muted text-xs text-justify leading-5 *:transition-all">
+          Este projeto foi desenvolvido com o objetivo de aplicar conceitos de desenvolvimento front-end. Ressalta-se que esta foi minha
+          primeira experiência com algumas tecnologias, tais como{' '}
+          <Link
+            to="https://tailwindcss.com/"
+            target="_blank"
+            className="text-primary/30 hover:text-primary hover:underline underline-offset-2"
+          >
+            TailwindCSS
+          </Link>
+          ,{' '}
+          <Link
+            to="https://www.typescriptlang.org/"
+            target="_blank"
+            className="text-primary/30 hover:text-primary hover:underline underline-offset-2"
+          >
+            TypeScript
+          </Link>
+          ,{' '}
+          <Link
+            to="https://reactrouter.com/"
+            target="_blank"
+            className="text-primary/30 hover:text-primary hover:underline underline-offset-2"
+          >
+            React Router
+          </Link>{' '}
+          e componentes do{' '}
+          <Link
+            to="https://ui.shadcn.com/"
+            target="_blank"
+            className="text-primary/30 hover:text-primary hover:underline underline-offset-2"
+          >
+            Shadcn
+          </Link>
+          .
+        </p>
+      </div>
     </footer>
   );
 }
