3234f7f5979c937dabb65765ca50d1ac3c206d5f
Patrick Franco <patrickxfranco@gmail.com>
2025-06-20 22:07:15 -0300

    Remove interfaces obsoletos de Actor, Movie e UserProps. Adiciona a interface ContentProps com a estrutura básica para o conteúdo.

---
+++ src/types/content.ts
@@ -0,0 +1,7 @@
+export interface ContentProps {
+  title: string;
+  type: string;
+  website: string;
+  cover: string;
+  description: string;
+}
--- src/types/user.ts
+++
@@ -1,7 +0,0 @@
-export interface UserProps {
-  username: string;
-  name: string;
-  surname: string;
-  email: string;
-  avatar: string;
-}
